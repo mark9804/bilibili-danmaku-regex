36b781e855374fd682e91734f30e87bb70ca103d
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2021-07-18 16:43:59 +0900

    测试工具能够自动检测并重加载文件

--- TestTool.py
+++ TestTool.py
@@ -1,21 +1,37 @@
 #coding:utf-8
 
 import regex
+import os
 
 regexPlainText = 'B站弹幕屏蔽正则.txt'
+timeModified = os.path.getmtime(regexPlainText)
 
 matchCompiled = {}
 matchPlainText = {}
 
-with open(regexPlainText, 'r') as file_plaintext:
-	lineNum = 1
-	for line in file_plaintext:
-		if line != '':
-			matchCompiled[lineNum] = regex.compile(line.rstrip('\n'))
-			matchPlainText[lineNum] = line.rstrip('\n')
-		lineNum += 1
+def loadfile():
+	global matchCompiled
+	global matchPlainText
+	with open(regexPlainText, 'r') as file_plaintext:
+		lineNum = 1
+		for line in file_plaintext:
+			if line != '':
+				matchCompiled[lineNum] = regex.compile(line.rstrip('\n'))
+				matchPlainText[lineNum] = line.rstrip('\n')
+			lineNum += 1
+
+def initialize():
+	global timeModified
+	timeModifiedCheck = os.path.getmtime(regexPlainText)
+	if timeModifiedCheck != timeModified:
+		loadfile()
+		timeModified = timeModifiedCheck
+		print('正则文件已更新')
+	else:
+		pass
 
 def testRegex(string):
+	initialize()
 	stringMatched = False
 	result = []
 	notInXmlList = []
@@ -38,6 +54,7 @@ def testRegex(string):
 	return result
 
 while True:
+	loadfile()
 	testResult = testRegex(input('输入需要测试的弹幕：'))
 	for i in testResult:
 		print(i)
