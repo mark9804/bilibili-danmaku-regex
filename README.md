3dd84fcef2322107948df0e536f5595d0be9e5fc
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2021-08-17 20:27:52 +0900

    正则测试工具支持分析json文件

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # bilibili-danmaku-regex
-结合[pakku.js](https://github.com/xmcp/pakku.js)使用的自用B站弹幕屏蔽正则
+结合[pakku.js](https://github.com/xmcp/pakku.js)使用的自用B站弹幕屏蔽正则，以及正则测试工具
 
-## 怎么用
+## 黑名单怎么用
 ### 1. 使用pakku黑名单（推荐）
 
 1. 全选并复制[Raw文件](https://raw.githubusercontent.com/Mark9804/bilibili-danmaku-regex/master/B%E7%AB%99%E5%BC%B9%E5%B9%95%E5%B1%8F%E8%94%BD%E6%AD%A3%E5%88%99.txt)（如果网络环境特殊打不开这个链接，请自行复制[txt文本](https://github.com/Mark9804/bilibili-danmaku-regex/blob/master/B%E7%AB%99%E5%BC%B9%E5%B9%95%E5%B1%8F%E8%94%BD%E6%AD%A3%E5%88%99.txt)中的内容）
@@ -19,3 +19,24 @@
 ⚠️XML文件是自动生成的，我没有测试过。如果出现应用文件后弹幕全部消失的情况请清除屏蔽列表中正则表达式内容；**为保证兼容性，XML文件不包含含有前后向断言的正则**
 
 另外推荐一个网站：[Bilibili 屏蔽词分享平台](https://harrynull.tech/bilibili/#sharelist) 本正则已集成其中大部分个人认为需要的规则。但是**网友的创造力是无穷的，遇到漏网弹幕也希望大家能够提出PR和[Issue](https://github.com/Mark9804/bilibili-danmaku-regex/issues)，共建美好B站**。
+
+## 正则测试工具怎么用
+
+### 测试单条弹幕：
+
+```
+python3 TestTool.py
+```
+
+### 批量测试弹幕json（可通过[the1812/Bilibili-Evolved](https://github.com/the1812/Bilibili-Evolved)导出）
+
+```
+python3 TestTool.py json文件路径
+```
+
+运行完成后，会提示结果保存地址：
+
+```
+被捕获弹幕已移送至[TestTool.py所在目录地址]/命中弹幕.csv
+```
+
--- TestTool.py
+++ TestTool.py
@@ -2,8 +2,12 @@
 
 import regex
 import os
+import sys
+import json
 
-regexPlainText = 'B站弹幕屏蔽正则.txt'
+currentPath = os.path.realpath(__file__).rstrip('/TestTool.py')
+
+regexPlainText = os.path.join(currentPath + '/B站弹幕屏蔽正则.txt')
 
 def loadfile():
 	global matchCompiled
@@ -26,8 +30,8 @@ def initialize():
 	else:
 		pass
 
-def testRegex(string):
-	initialize()
+def testRegex(string, mode='single'):
+	initialize() if mode == 'single' else False
 	stringMatched = False
 	result = []
 	notInXmlList = []
@@ -47,8 +51,11 @@ def testRegex(string):
 		for i in notInXmlList:
 			result.insert(len(tempResult), str('第' + str(i) + '条正则不在XML文件中'))
 	if stringMatched == False:
-		result = ['该弹幕没有被正则捕获']
 		matchedRules = 0
+		if mode=='single':
+			result = ['该弹幕没有被正则捕获']
+		else:
+			result = False
 	return result, matchedRules
 
 
@@ -58,8 +65,38 @@ matchCompiled = {}
 matchPlainText = {}
 
 loadfile()
-while True:
-	testResult, matched = testRegex(str(input('输入需要测试的弹幕：')))
-	for i in testResult:
-		print(i)
-	print('共命中' + str(matched) + '条正则\n')
+if len(sys.argv) == 1:
+	while True:
+		testResult, matched = testRegex(str(input('输入需要测试的弹幕：')))
+		for i in testResult:
+			print(i)
+		print('共命中' + str(matched) + '条正则\n')
+else:
+	danmakuPath = ''
+	for i in sys.argv:
+		if regex.search(r'.json$', i) is not None:
+			matchingRules = {}
+			danmakuPath = i
+			print('正在分析' + danmakuPath + '：')
+			with open(danmakuPath, 'r') as f:
+				danmakus = f.read()
+				f.close()
+			jsonfile = json.loads(danmakus)
+			for danmaku in jsonfile:
+				testResult, matched = testRegex(danmaku['content'],mode='batch')
+				if testResult is not False:
+					matchingRules[danmaku['content']] = matched
+			with open(os.path.join(currentPath + '/命中弹幕.csv'),'w') as f:
+				f.write('弹幕内容,命中次数\n')
+			with open(os.path.join(currentPath + '/命中弹幕.csv'),'a') as f:
+				for key,value in dict.items(matchingRules):
+					f.write(str(key.replace(',', '（英文逗号）')) + ',' + str(value) + '\n')
+				f.close()
+			print('共捕获' + str(len(matchingRules)) + '条弹幕；被捕获弹幕已移送至' + str(os.path.join(currentPath + '/命中弹幕.csv')))
+	if danmakuPath == '':
+		print('未找到需要分析的弹幕文件路径！')
+		while True:
+			testResult, matched = testRegex(str(input('输入需要测试的弹幕：')))
+			for i in testResult:
+				print(i)
+			print('共命中' + str(matched) + '条正则\n')
